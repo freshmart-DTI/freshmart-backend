08a5d4fe063ae8eac43963717e4991d928be1887
Rizky Ramadhan <38162104+rizkyr99@users.noreply.github.com>
2024-10-04 19:30:56 +0700

    refactor(store): update native query to use 'stores' table name

--- src/main/java/com/freshmart/backend/store/repository/StoreRepository.java
+++ src/main/java/com/freshmart/backend/store/repository/StoreRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
-    @Query(value = "SELECT * FROM store " +
+    @Query(value = "SELECT * FROM stores " +
             "ORDER BY POWER(latitude - :lat, 2) + POWER(longitude - :lon, 2) ASC " +
             "LIMIT 1", nativeQuery = true)
     Store findNearestStore(@Param("lat") double latitude, @Param("lon") double longitude);
